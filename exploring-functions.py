d65ff1b05c044e8172ef634c3521ea8590f0af36
ForresterCW <forrestercrawfordweishar@gmail.com>
2024-08-16 22:22:01 -0700

    Setup environment and exploring functions

---
+++ exploring-functions.py
@@ -0,0 +1,8 @@
+## To learn what OOP functions look like, by building them.
+
+def f_myFirstFunction(userName):
+    print("Hello " + userName + " it is nice to meet you, welcome to Object Oriented Programming.")
+    return
+
+f_myFirstFunction("Forrester")
+
---
+++ main.py
@@ -0,0 +1,2 @@
+## To build my version of the CVM algorithm, and explore and visualize the error rates and the impact of the law of large numbers, while comparing it to a baseline simple unique value counting algorithm.
+
