fc47db7f44bb966bef3e70e297479b8aa60e05e7
ForresterCW <forrestercrawfordweishar@gmail.com>
2024-08-16 23:22:02 -0700

    Sidetracked by the averages of large numbers

---
+++ averages-of-large-numbers.py
@@ -0,0 +1,28 @@
+# Goal: To see how the average changes as the number of random numbers increases.
+## Prediction: As random numbers -> inf, avg / (source list /2) = 1
+
+# Importing modules
+import random
+
+# Simulate a unique data stream into a source list
+def f_generateSourceList(targetList, size):
+    for i in range(size):
+        randomNumber = random.randint(1,size)
+        targetList.append(randomNumber)
+    return
+
+# Average a list
+def f_averageList(listToAverage, averageStorage):
+    average = sum(listToAverage) / len(listToAverage)
+    round(average, 1)
+    averageStorage.append(average)
+
+# 
+randomAverages = []
+
+for i in range(1,11):
+    sourceList = []
+    f_generateSourceList(sourceList, i)
+    print(sourceList)
+    f_averageList(sourceList, randomAverages)
+    print(f"List {i} average = {randomAverages[-1]}")
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-## To build my version of the CVM algorithm, and explore and visualize the error rates and the impact of the law of large numbers, while comparing it to a baseline simple unique value counting algorithm.
+# To build my version of the CVM algorithm, and explore and visualize the error rates and the impact of the law of large numbers, while comparing it to a baseline simple unique value counting algorithm.
 
+## Importing modules
+import random
+
+## Simulating a unique data stream into a source list
+sourceList = []
+def f_generateSourceList(targetList, size):
+    for i in range(size):
+        randomNumber = random.randint(1,size)
+        targetList.append(randomNumber)
+    return
+
+## Algorithm Main Loop
+f_generateSourceList(sourceList, 10)
+print(sourceList)
