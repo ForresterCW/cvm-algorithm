a451948abb9d771a789aed06fbc8e15ee7513a40
ForresterCW <forrestercrawfordweishar@gmail.com>
2024-08-22 13:13:20 -0700

    Black formatting

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def f_simple_Counting_algorithm(source_list, memory_limit):
         if debugging_state:
             print(unique_items_list)
 
-        # Check memory limit        
+        # Check memory limit
         if len(unique_items_list) >= memory_limit:
             if debugging_state:
                 print(f"{unique_items_list} at max memory exit.")
@@ -46,26 +46,26 @@ probability_scaling_factor = 0.5
 ## Generate Source List
 source_list = f_generate_source_list(source_list_size, source_max_int)
 
-## Baseline Counting Algorithm 
+## Baseline Counting Algorithm
 # baseline_unique_items_list = f_simple_Counting_algorithm(source_list, memory_limit)
 
 ## Advanced CVM algorithm
 
 
-
 # Workshop
 # Advanced CVM algorithm with memory checking and debug display
 
+
 def f_OUTDATED_cvm_algorithm(source_list, memory_limit, probability_factor):
     if probability_factor == 1:
-        # Simple counting algorithm 
+        # Simple counting algorithm
         unique_items_list = []
         for item in source_list:
             # Debug display list growth
             if debugging_state:
                 print(unique_items_list)
 
-            # Check memory limit        
+            # Check memory limit
             if len(unique_items_list) >= memory_limit:
                 if debugging_state:
                     print(f"{unique_items_list} at max memory exit.")
@@ -75,7 +75,7 @@ def f_OUTDATED_cvm_algorithm(source_list, memory_limit, probability_factor):
             # Add item to unique_items_list if it isnt there
             if item not in unique_items_list:
                 unique_items_list.append(item)
-    
+
     # Iterate probability factor
     probability_factor = probability_factor * probability_scaling_factor
     print(f"Memory Full, new probability factor is {probability_factor}")
@@ -94,11 +94,11 @@ def f_cvm_algorithm(source_list, memory_limit, probability_factor):
         if debugging_state:
             print(unique_items_list)
 
-        # Check memory limit        
+        # Check memory limit
         if len(unique_items_list) >= memory_limit:
             if debugging_state:
                 print(f"{unique_items_list} at max memory exit.")
             print("!!! MAX MEMORY !!!")
-                
+
 
 f_cvm_algorithm(source_list, memory_limit, probability_factor)
